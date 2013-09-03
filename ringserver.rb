8d7d6192bfe598bd05afb2bcdedc9e499f91e24e
Davy Stevenson <davy.stevenson@gmail.com>
2013-09-02 18:38:52 -0700

    Swap out ringserver with RingyDingy::RingServer

--- ringserver.rb
+++ ringserver.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby -w
 
-require 'rinda/ring'
-require 'rinda/tuplespace'
+require 'ringy_dingy/ring_server'
 
-DRb.start_service
+puts "You should really use ringy_dingy's ring_server"
+puts "Starting RingServer for you anyway!"
+RingyDingy::RingServer.run
 
-Rinda::RingServer.new Rinda::TupleSpace.new
-
-DRb.thread.join
