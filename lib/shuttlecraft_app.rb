326293caf58cad4fec5a0ae277f66913b3ad3b0d
Davy Stevenson <davy.stevenson@gmail.com>
2013-10-03 16:52:05 -0700

    Adding ability to set shuttlecraft name to shuttlecraft_app

--- lib/shuttlecraft_app.rb
+++ lib/shuttlecraft_app.rb
@@ -4,16 +4,33 @@ Shoes.app :width => 360, :height => 360, :resizeable => false do
 
   @shuttlecraft = nil
 
-  stack :margin => 20 do
-    title "Shuttlecraft"
+  def display_screen
+    clear do
+      stack :margin => 20 do
+        title "Shuttlecraft"
 
-    stack do @status = para end
+        stack do @status = para end
 
-    button("Register") { register }
-    button("Unregister") { unregister }
+        button("Register") { register }
+        button("Unregister") { unregister }
+      end
+      animate(5) {@status.replace status_text}
+    end
   end
 
-  animate(5) {@status.replace status_text} 
+
+  def registration_screen
+    clear do
+      background black
+      el = edit_line text: 'Name' do |s|
+        @name = s.text
+      end
+      button('register') {
+        @shuttlecraft = Shuttlecraft.new(@name)
+        display_screen
+      }
+    end
+  end
 
   def status_text
     "Registered? #{@shuttlecraft.registered? if @shuttlecraft}"
@@ -27,6 +44,5 @@ Shoes.app :width => 360, :height => 360, :resizeable => false do
     @shuttlecraft.unregister if @shuttlecraft
   end
 
-  @shuttlecraft = Shuttlecraft.new
-
+  registration_screen
 end
